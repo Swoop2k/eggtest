cdf6646371b0c592ac6e2b439549df2519c23940
swoop2k <dsr1972@gmail.com>
2023-03-21 20:18:38 -0300

    mensaje

---
+++ JavaApplication1-EGG/EGG_Cooperativo/src/egg_cooperativo/Ejercicio1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egg_cooperativo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author swoop
+ */
+public class Ejercicio1 {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("ingrese un nro");
+        double num = input.nextDouble();
+        if ( num>500 ) {
+            System.out.println();}
+            else {
+            
+            
+        }
+        
+    }
+    
+}
---
+++ JavaApplication1-EGG/EGG_Cooperativo/src/egg_cooperativo/Ejercicio9.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package egg_cooperativo;
+
+/**
+ *
+ * @author swoop
+ */
+public class Ejercicio9 {
+    
+
+public static void main(String[] args) {
+   	 
+    	for (int i = 0; i < 10; i++) {
+
+    	System.out.println("Imprimo el valor de i: " + i);
+
+    	}
+   	 
+    	System.out.println("===================================");
+
+    	System.out.println("For decreciendo");
+   	 
+    	for (int i = 10; i > 10; i--) {
+
+    	System.out.println("Imprimo el valor de i: " + i);
+
+    	}
+}
+}
